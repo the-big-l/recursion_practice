3cbc85866989d394c65a0de8ba2b8fcac6ab36cc
Leo Salat <leo.salat@gmail.com>
2017-01-04 21:21:11 -0600

    added additional problems to basic

--- solutions/basic_recursion.rb
+++ solutions/basic_recursion.rb
@@ -1,5 +1,44 @@
+def factorial(n)
+  return 1 if n.zero?
+
+  n * factorial(n - 1)
+end
+
+def happy_feet(n)
+  return 0 if n.zero?
+
+  2 + happy_feet(n - 1)
+end
+
 def multiply(x, y)
-  return 0 if y == 0
+  return 0 if y.zero?
 
   x + multiply(x, y - 1)
+end
+
+def fibonacci(n)
+  return 0 if n <= 1
+  return 1 if n == 2
+
+  fibonacci(n - 1) + fibonacci(n - 2)
+end
+
+def unhappy_feet(n)
+end
+
+def triangle(n)
+end
+
+def sumDigits
+end
+
+def count7(n)
+end
+def count8(n)
+end
+
+def powerN(n)
+end
+
+def countX(n)
 end
--- spec/basic_recursion_spec.rb
+++ spec/basic_recursion_spec.rb
@@ -1,10 +1,40 @@
 require 'rspec'
-require 'basic_recursion'
+# uncomment below line and delete this line for final
+# require 'basic_recursion'
+require_relative '../solutions/basic_recursion'
 
 describe 'Recursion' do
-  describe 'Multiply' do
-    it 'multiplies two numbers together' do
+  context 'The factorial method' do
+    it 'calculates factorial of n.' do
+      expect(factorial(0)).to eq(1)
+      expect(factorial(1)).to eq(1)
+      expect(factorial(5)).to eq(120)
+    end
+  end
+
+  context 'The happy_feet method' do
+    it 'counts the number of penguin feet.' do
+      expect(happy_feet(0)).to eq(0)
+      expect(happy_feet(1)).to eq(2)
+      expect(happy_feet(11)).to eq(22)
+    end
+  end
+
+  context 'The multiply method' do
+    it 'multiplies two numbers together.' do
+      expect(multiply(0, 5)).to eq(0)
+      expect(multiply(5, 0)).to eq(0)
       expect(multiply(2, 5)).to eq(10)
+      expect(multiply(7, 7)).to eq(49) # go 'niners!
+    end
+  end
+
+  context 'The fibonacci method' do
+    it 'returns the nth fibonacci number.' do
+      expect(fibonacci(1)).to eq(0)
+      expect(fibonacci(2)).to eq(1)
+      expect(fibonacci(8)).to eq(13)
+      expect(fibonacci(13)).to eq(144)
     end
   end
 end
